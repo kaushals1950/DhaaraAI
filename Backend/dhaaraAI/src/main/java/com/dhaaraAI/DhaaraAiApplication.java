52acea70f85f60d6ae7443a3689ef238b5f842f6
Kaushal Singh <kaushals1950@gmail.com>
2025-09-08 01:02:45 +0530

    first commit

---
+++ Backend/dhaaraAI/src/main/java/com/dhaaraAI/DhaaraAiApplication.java
@@ -0,0 +1,13 @@
+package com.dhaaraAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DhaaraAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DhaaraAiApplication.class, args);
+	}
+
+}
---
+++ Backend/dhaaraAI/src/test/java/com/dhaaraAI/DhaaraAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhaaraAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhaaraAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
